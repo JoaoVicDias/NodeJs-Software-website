c7a4e3e91467a196e2e716f07c68cfd3b7a42fb8
João Victor <joovictordias@hotmail.com>
2020-06-10 20:01:05 -0300

    update2

--- app.js
+++ app.js
@@ -2,26 +2,49 @@ const express = require('express')
 const app = express()
 const bodyparser = require('body-parser')
 const path = require('path')
-const router = express.Router()
+const handlebars = require('express-handlebars')
 const mongoose = require('mongoose')
 require('./models/account')
 const account = mongoose.model('account')
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
 require('./config/auth')(passport)
+const session = require('express-session')
+const flash = require('connect-flash')
+const router = require('./routes/mainRouter')
 
-
+//Setting session
+app.use(session({
+    secret:"softwarewebsite",
+    resave:true,
+    saveUninitialized:true
+}))
 
 //Setting passport
 app.use(passport.initialize())
 app.use(passport.session())
 
+//Setting flash
+app.use(flash())
+app.use((req,res,next)=>{
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    res.locals.error = req.flash('error')
+    res.locals.user = req.user || null
+    next()
+})
+
 //Statics files
 app.use(express.static('public'))
+
+//Setting view engine
+app.engine('handlebars',handlebars({defaultLayout:'main'}))
+app.set('view engine', 'handlebars')
 
 //setting  Body-parser
 app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
+
 //Setting MongoDB
 mongoose.connect("mongodb://localhost:27017/robo3t",{useNewUrlParser: true,useUnifiedTopology:true}).then(()=>{
     console.log("MongoDB is running ...")
@@ -29,105 +52,8 @@ mongoose.connect("mongodb://localhost:27017/robo3t",{useNewUrlParser: true,useUn
     console.log("Error to connect MongoDB",err)
 })
 
-//Creating the Main route
-app.use("/",router)
-
-
-
-
-
-//Creating routes
-router.get("/",(req,res) =>{
-    res.sendFile(path.join(__dirname+"/public/site.html"))
-})
-
-
-router.get("/Download",(req,res) => {
-    res.sendFile(path.join(__dirname+"/public/download.html"))
-})
-
-
-router.get("/Aboutme",(req,res)=> {
-    res.sendFile(path.join(__dirname+'/public/aboutme.html'))
-})
-
-router.get("/Account",(req,res) => {
-    res.sendFile(path.join(__dirname+'/public/account.html'))
-})
-
-router.get("/CreatingAccount",(req,res) => {
-    res.sendFile(path.join(__dirname+"/public/createAccount.html"))
-})
-
-router.post("/CreatingAccount",(req,res)=>{
-    
-    if(!req.body.name || typeof req.body.name == undefined || req.body.nome == null ){
-        
-    }
-
-    if(!req.body.email || typeof req.body.email == undefined || req.body.email == null ){
-        
-    }
-
-    if(!req.body.password || typeof req.body.password == undefined || req.body.password == null ){
-        
-    }
-
-    if(req.body.password.length < 6){
-        
-    }
-
-    if(req.body.password != req.body.password2){
-        
-    }else{
-        account.findOne({email: req.body.email}).then((Account)=>{
-            if (Account){
-               
-            }else{
-                const newAccount = new account({
-                    name: req.body.name,
-                    email:req.body.email,
-                    password:req.body.password
-                })
-
-                bcrypt.genSalt(10,(erro,salt)=>{
-                    bcrypt.hash(newAccount.password, salt, (erro,hash)=>{
-                        newAccount.password = hash
-                        newAccount.save().then(()=>{
-                            res.redirect("/")
-                            console.log('Account created')
-                        })
-                    })
-                })
-               
-        
-            }
-        })
-    }
-    
-    
-})
-
-router.post("/Account",(req,res,next)=>{
-    passport.authenticate("local",{
-        successRedirect:"/",
-        failureRedirect:"/Account",
-    })
-    
-    
-})
-
-router.get("/Logout",(req,res)=>{
-    req.logOut()
-    res.redirect("/")
-})
-
-
-
-
-
-
-
+//Calling the route
+app.use(router)
 
 
 
